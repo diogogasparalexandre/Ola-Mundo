47eefffe7cfe5f4bd9bb0828981707fda2899990
Diogo Gaspar <diogogaspar.trabalho@gmail.com>
2025-01-09 17:09:00 +0000

    Update To do.py
    Change def ver_tarefas, adicionar_tarefas,menu and while

--- Projetos pessoais/To do.py
+++ Projetos pessoais/To do.py
@@ -8,19 +8,20 @@ def menu():
     print('1 - Ver tarefas'.ljust(40))
     print('2 - Adicionar tarefa'.ljust(40))
     print('3 - Remover tarefa'.ljust(40))
-    print('4 - Sair'.ljust(40))
+    print('4 - Modificar tarefa'.ljust(40))
+    print('5 - Sair'.ljust(40))
     print('-'*40)
 
-#Recolher a escolha do usuário
 
+#Recolher a escolha do usuário
 def ver_tarefas():
     if len(tarefas) == 0:
         print('Nenhuma tarefa CADASTRADA')
     else:
-        for tarefa in tarefas:
-            print(tarefa)
-            
-#Adicionar tarefa
+        for i, tarefa in enumerate(tarefas):
+            status = 'Concluida' if tarefa['Concluida'] else 'Pendente'
+            print(f'{i+1} - {tarefa["Nome"]} - {status}')
+
 def adicionar_tarefa():
     tarefa = input('Digite a tarefa: ').title().strip()
     if any(t['Nome'] == tarefa for t in tarefas):
@@ -29,12 +30,11 @@ def adicionar_tarefa():
     tarefas.append({'Nome': tarefa, 'Concluida': False})
     print('({}) adicionada com sucesso!'.format(tarefa))
     
-#Menu principal
 tarefas = []
 while True:
     menu()
     try:
-        escolha = int(input('Escolha uma option: '))
+        escolha = int(input('Escolha uma opção: '))
         if escolha == 1:
             print('Ver tarefas')
             ver_tarefas()
@@ -43,12 +43,13 @@ while True:
         elif escolha == 3:
             print('Remover tarefa')
         elif escolha == 4:
+            print('Modificar tarefa')
+        elif escolha == 5:
             print('Sair')
             break
         else:
             print('Opção inválida. Por favor, escolha uma opção válida.')
     except ValueError:
         print('Opção inválida. Por favor, escolha uma opção válida.')
-
 
 
