bc0461c93407d63b9c401b4d0706970e083730cd
Diogo Gaspar <diogogaspar.trabalho@gmail.com>
2025-01-09 12:34:34 +0000

    Create To do.py

---
+++ Projetos pessoais/To do.py
@@ -0,0 +1,10 @@
+def menu():
+    print('-'*40)
+    print('LISTA DE TAREFAS'.center(40))
+    print('-'*40)
+    print('1 - Ver tarefas'.ljust(40))
+    print('2 - Adicionar tarefa'.ljust(40))
+    print('3 - Remover tarefa'.ljust(40))
+    print('4 - Sair'.ljust(40))
+    print('-'*40)
+
